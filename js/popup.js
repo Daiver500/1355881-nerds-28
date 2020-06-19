8cc0a3e282ac8e536bea0298335775409000fe95
Daiver500 <60549770+Daiver500@users.noreply.github.com>
2020-06-19 17:26:39 +0300

    Попап (открыть\закрыть)

--- js/popup.js
+++ js/popup.js
@@ -2,22 +2,12 @@ let openPopup = document.querySelector(".write-us-button");
 let popupForm = document.querySelector(".popup");
 let closePopup = document.querySelector(".close-button");
 
-/*openPopup.addEventListener("click", function () {
-    evt.preventDefault(); 
-    popupForm.classList.add(".popup-show");
-});
-
-closePopup.addEventListener("click", function () {
-    evt.preventDefault(); 
-    popupForm.classList.remove(".popup-show");
-});*/
-
-openPopup.onclick = function () {
-    /*evt.preventDefault();*/
-    popupForm.classList.add(".popup-show");
+openPopup.onclick = function (evt) {
+    evt.preventDefault();
+    popupForm.classList.add("popup-show");
 };
 
-closePopup.onclick = function () {
-    /*evt.preventDefault(); */
-    popupForm.classList.remove(".popup-show");
+closePopup.onclick = function (evt) {
+    evt.preventDefault(); 
+    popupForm.classList.remove("popup-show");
 };
