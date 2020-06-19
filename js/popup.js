1f384cb27839cbddd4e35bbc4e08be491489f4b0
Daiver500 <60549770+Daiver500@users.noreply.github.com>
2020-06-19 15:45:08 +0300

    JS пока не работает:(

---
+++ js/popup.js
@@ -0,0 +1,13 @@
+let openPopup = document.querySelector(".write-us-button");
+let popupForm = document.querySelector(".popup");
+let closePopup = document.querySelector(".close-button");
+
+openPopup.addEventListener("click", function () {
+    evt.preventDefault(); 
+    popupForm.classList.add(".popup-show");
+});
+
+closePopup.addEventListener("click", function () {
+    evt.preventDefault(); 
+    popupForm.classList.remove(".popup-show");
+});
